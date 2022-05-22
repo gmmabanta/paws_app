b6b99741c502383ffd370e5ebb059a17b6a23d3e
Mae Mabanta <gmmabanta@gmail.com>
2022-05-22 22:09:09 +0800

    routing and cleanup

--- paws_app/frontend/src/components/login/index.js
+++ paws_app/frontend/src/components/login/index.js
@@ -32,33 +32,13 @@ const FormWrapper = styled.div`
     align-items: center;
 `;
 
-const todoItems = [
-  {
-    id: 1,
-    title: "Nature walk in the park",
-    description: "Visit the park with my friends",
-    completed: true
-  },
-
-  {
-    id: 2,
-    title: "Visit",
-    description: "Got to my aunt's place",
-    completed: true
-  },
-
-  {
-    id: 3,
-    title: "Write",
-    description: "Do an article about anthropology",
-    completed: true
-  },
-];
-
 class Login extends Component {
     constructor(props) {
       super(props);
-      this.state = {todoItems};
+      this.state = {
+        username: '',
+        password: ''
+      };
     };
 
     render() {
@@ -68,31 +48,12 @@ class Login extends Component {
                     <img src="http://thecreativecat.net/wp-content/uploads/PAWS-logo.jpg" alt="PAWS" height='200px'/>
                 </LogoWrapper>
                     <FormWrapper>
-                        <FieldInput fieldname="Username" />
-                        <FieldInput fieldname="Password" />
-                        <LoginButton>LOGIN</LoginButton>
-                    </FormWrapper>
-                
-                                
+                        <FieldInput id="username" fieldname="Username" type="text" />
+                        <FieldInput fieldname="Password" type="password" />
+                        <LoginButton type="submit" >LOGIN</LoginButton>
+                    </FormWrapper>        
             </LoginWrapper>
-    //     <main className="content">
-    //     <LoginButton>Button</LoginButton>
-    //     <div className="row">
-    //       <div className="col-md-6 col-sm-10 mx-auto p-0">
-    //         <div className="card p-3">
-    //           <ul className="list-group list-group-flush">
-    //           {this.state.todoItems.map(item => (
-    //           <div>
-    //             <h1>{item.title}</h1>
-    //             <span>{item.description}</span>
-    //           </div>
-    //           ))}
-    //           </ul>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </main>
-      )
+        )
     }
   }
   
