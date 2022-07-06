c093c3ad5a727543ae97bfb582c6c3c1a65aa5f9
hpascua <43760125+hpascua@users.noreply.github.com>
2022-07-06 20:39:39 +0800

    Added urls

--- paws_app/backend/backend/urls.py
+++ paws_app/backend/backend/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from paws_db import uauth
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('uauth/', uauth.userLogin)
 ]
