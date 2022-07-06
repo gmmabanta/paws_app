6ccaf22628e7eb7069df99da17b65b3923bcf3d0
hpascua <43760125+hpascua@users.noreply.github.com>
2022-07-06 20:41:58 +0800

    Update models.py

--- paws_app/backend/paws_db/models.py
+++ paws_app/backend/paws_db/models.py
@@ -1,8 +1,12 @@
+from tkinter import CASCADE
 from turtle import setx
 from unicodedata import name
 from django.db import models
 
 class Client(models.Model):
+    class Meta:
+        db_table = 'client'
+
     frst_name = models.CharField(max_length=80)
     lst_name = models.CharField(max_length=40)
     addr = models.CharField(max_length=200)
@@ -10,17 +14,36 @@ class Client(models.Model):
     fb = models.CharField(max_length=40)
     remarks = models.CharField(max_length=500)
 
+class RescueDetail(models.Model):
+    class Meta:
+        db_table = 'rescue_detail'
+
+    rescuer = models.ForeignKey(Client, blank=True, null=True, on_delete=models.SET_NULL)
+    addr = models.CharField(max_length=200)
+    remarks = models.CharField(max_length=200)
+    with_legal_case = models.BooleanField()
+
+class FosterDetail(models.Model):
+    class Meta:
+        db_table = 'foster_detail'
+
+    fosterer = models.ForeignKey(Client, blank=True, null=True, on_delete=models.SET_NULL)
+    rel_date = models.DateField()
+    ret_date = models.DateField()
+
 class Animal(models.Model):
+    class Meta:
+        db_table = 'animal'
+
     SEX_CHOICES = [
         ('M', 'Male')
        ,('F', 'Female')
     ]
 
     BREED_SPECIES = [
-        """
-        Place list of breeds here as tuples: (<Breed>, <Specie>)
-        e.g. ('Shih Tzu', 'Dog')
-        """
+        #Place list of breeds here as tuples: (<Breed>, <Specie>)
+        #e.g. ('Shih Tzu', 'Dog')
+        
         ('Aspin', 'Dog')
        ,('Puspin', 'Cat')
     ]
@@ -30,42 +53,35 @@ class Animal(models.Model):
     fost_parc = models.CharField(max_length=80)
     age = models.IntegerField(blank=True, null=True)
     sex = models.CharField(max_length=1, choices=SEX_CHOICES)
-    breed = models.CharField(choices=BREED_SPECIES)
+    breed = models.CharField(max_length=30, choices=BREED_SPECIES)
     desc = models.CharField(max_length=1000)
     hist = models.CharField(max_length=1000)
-    rescue_det = models.ForeignKey(RescueDetail.id, blank=True, null=True)
-    foster_det = models.ForeignKey(FosterDetail.id, blank=True, null=True)
+    rescue_det = models.ForeignKey(RescueDetail, blank=True, null=True, on_delete=models.SET_NULL)
+    foster_det = models.ForeignKey(FosterDetail, blank=True, null=True, on_delete=models.SET_NULL)
     init_wt = models.DecimalField(max_digits=5, decimal_places=2)
 
 class ClientContact(models.Model):
+    class Meta:
+        db_table = 'client_contact'
+
     CONTACT_CHOICES = [
         ('E', 'Email'),
         ('T', 'Telephone'),
     ]
 
-    client_id = models.ForeignKey(Client.id)
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
     contact_type = models.CharField(max_length=1, choices=CONTACT_CHOICES)
     contact = models.CharField(max_length=40)
 
-class RescueDetail(models.Model):
-    shelt_anim = models.ForeignKey(Animal.id)
-    rescuer = models.ForeignKey(Client.id, blank=True, null=True)
-    addr = models.CharField(max_length=200)
-    remarks = models.CharField(max_length=200)
-    with_legal_case = models.BooleanField()
-
-class FosterDetail(models.Model):
-    shelt_anim = models.ForeignKey(Animal.id)
-    fosterer = models.ForeignKey(Client.id, blank=True, null=True)
-    rel_date = models.DateField()
-    ret_date = models.DateField()
-
-class MedHist(models.Model):
-    anim = models.ForeignKey(Animal.id)
-    date = models.DateField()
-    wt = models.DecimalField(max_digits=5, decimal_places=2)
-    ## TODO: add details for findings, vaccination, deworming, spay/neuter
+# class MedHist(models.Model):
+#     anim = models.ForeignKey(Animal.id, on_delete=models.CASCADE)
+#     date = models.DateField()
+#     wt = models.DecimalField(max_digits=5, decimal_places=2)
+#     ## TODO: add details for findings, vaccination, deworming, spay/neuter
 
 class UserAuth(models.Model):
-    uname = models.CharField(primary_key=True)
-    pw = models.CharField()
+    class Meta:
+        db_table = 'user_auth'
+
+    uname = models.CharField(primary_key=True, max_length=50, blank=False, null=False)
+    pw = models.CharField(max_length=50, blank=False, null=False)
