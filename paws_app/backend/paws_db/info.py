c13037ef8ac66ffda4c500abc5b8e528d4a47593
Harold Luis Pascua <43760125+hpascua@users.noreply.github.com>
2022-08-15 21:49:38 +0800

    Update info.py

--- paws_app/backend/paws_db/info.py
+++ paws_app/backend/paws_db/info.py
@@ -13,10 +13,55 @@ def userExists (id):
         return False
 
 # Insert new information (about an animal)
+def addInfo (request):
+    animalTag = 'animalID'
+
+    if animalTag not in request.headers:
+        return HttpResponseBadRequest("Information addition failed: missing parameters")
+
+    animalId = request.headers[animalTag]
+
+    if animalExists(animalId):
+        return HttpResponseBadRequest("Information addition failed: animal already exists")
+
+    # TODO: Insert animal and return response
+    response = HttpResponse("Information deletion successful")
+
+    return response
 
 # Modify information
+def modifyInfo (request):
+    animalTag = 'animalID'
+
+    if animalTag not in request.headers:
+        return HttpResponseBadRequest("Information modification failed: missing parameters")
+
+    animalId = request.headers[animalTag]
+
+    if not animalExists(animalId):
+        return HttpResponseBadRequest("Information modification failed: animal does not exist")
+
+    # TODO: Modify animal and return response
+    response = HttpResponse("Information modification successful")
+
+    return response
 
 # Delete information
+def deleteInfo (request):
+    animalTag = 'animalID'
+
+    if animalTag not in request.headers:
+        return HttpResponseBadRequest("Information deletion failed: missing parameters")
+
+    animalId = request.headers[animalTag]
+
+    if not animalExists(animalId):
+        return HttpResponseBadRequest("Information deletion failed: animal does not exist")
+
+    # TODO: Delete animal and return response
+    response = HttpResponse("Information deletion successful")
+
+    return response
 
 # Retrieve information to display
 def getInfo (request):
