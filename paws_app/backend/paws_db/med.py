01e7ffb7ef977a7415646d4920f5fd234d83593a
Harold Luis Pascua <43760125+hpascua@users.noreply.github.com>
2022-08-16 14:22:37 +0800

    Setup function templates

--- paws_app/backend/paws_db/med.py
+++ paws_app/backend/paws_db/med.py
@@ -1,5 +1,9 @@
 # MEDICAL HISTORY PAGE FUNCTIONS
 
 # Insert an entry
+def insertEntry (request):
+    pass
 
 # Retrieve all entries for an animal
+def getEntries (request):
+    pass
--- paws_app/backend/paws_db/rescue.py
+++ paws_app/backend/paws_db/rescue.py
@@ -1,9 +1,17 @@
 # RESCUE PAGE FUNCTIONS
 
 # Insert rescue info
+def addRescInfo (request):
+    pass
 
 # Modify rescue info
+def modifyRescInfo (request):
+    pass
 
 # Delete rescue info
+def deleteRescInfo (request):
+    pass
 
 # Get rescue info for an animal
+def getRescInfo (request):
+    pass
